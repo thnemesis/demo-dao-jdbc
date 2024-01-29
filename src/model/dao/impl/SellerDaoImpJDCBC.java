add51184fc2ecca174d43dc7be95ba5bbda8cdee
Thiago Silva <thnemesis@outlook.com>
2024-01-28 23:55:59 -0300

    iniciando classes de acesso ao JDBC

---
+++ src/model/dao/impl/SellerDaoImpJDCBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoImpJDCBC implements SellerDao{
+
+	@Override
+	public void insert(Seller obj) {
+		
+	}
+
+	@Override
+	public void upadate(Seller obj) {
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		return null;
+	}
+
+}
